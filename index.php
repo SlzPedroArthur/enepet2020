93e22008d586bd801b1f4704f533e6f42679d944
SlzPedroArthur <56669563+SlzPedroArthur@users.noreply.github.com>
2020-01-17 10:02:27 -0300

    update do slider

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
-                            <h1 class="m-b-20"><strong>Edital de Submissão<br>de Trabalhos!</strong></h1>
-                            <p class="m-b-40"> Clique aqui para <br> acessar o edital.</p>
-                            <p><a class="btn hvr-hover" href="https://even3.blob.core.windows.net/geral/EditaldeSubmissodeResumosExpandidos-XIXENEPET.bd73bb80dd104635ad42.pdf">Edital</a></p>
+                            <h1 class="m-b-20"><strong>Edital de <br>Inscriçoes!</strong></h1>
+                            <p class="m-b-40"> Clique aqui para <br> acessar o edital!</p>
+                            <p><a class="btn hvr-hover" href="https://drive.google.com/open?id=15UUWE7sN5_m4Q3KC5UoHjg5O5DWsMVnR">Edital</a></p>
                         </div>
                     </div>
                 </div>
@@ -32,9 +32,9 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
-                            <h1 class="m-b-20"><strong>Últimas <br> Notícias</strong></h1>
-                            <p class="m-b-40">Depois de reunião com <br> nosso reitor temos novidades.</p>
-                            <p><a class="btn hvr-hover" href="https://www.even3.com.br/enepet2020/">Saiba mais!</a></p>
+                            <h1 class="m-b-20"><strong>Edital de Submissão <br> de Trabalhos.</strong></h1>
+                            <p class="m-b-40">Clique no link<br>abaixo para ter acesso.</p>
+                            <p><a class="btn hvr-hover" href="https://even3.blob.core.windows.net/geral/EditaldeSubmissodeResumosExpandidos-XIXENEPET.bd73bb80dd104635ad42.pdf">Saiba mais!</a></p>
                         </div>
                     </div>
                 </div>
@@ -59,6 +59,45 @@
                 </div>
             </div>
             <div class="row">
+                <div class="col-md-6 col-lg-4 col-xl-4">
+                    <div class="blog-box">
+                        <div class="blog-content">
+                            <div class="title-blog">
+                                <h3>Inscrições abertas!</h3>
+                                <p>UFMA/Notícias: UFMA sediará em 2020 o XIX Encontro Nordestino dos Grupos do Programa de Educação Tutorial.</p>
+                            </div>
+                            <ul class="option-blog">
+                                <li><a href="https://portais.ufma.br/PortalUfma/paginas/noticias/noticia.jsf?id=54097" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 col-xl-4">
+                    <div class="blog-box">
+                        <div class="blog-content">
+                            <div class="title-blog">
+                                <h3>Notícias vindo da reitoria</h3>
+                                <p>UFMA/Notícias: UFMA sediará em 2020 o XIX Encontro Nordestino dos Grupos do Programa de Educação Tutorial.</p>
+                            </div>
+                            <ul class="option-blog">
+                                <li><a href="https://portais.ufma.br/PortalUfma/paginas/noticias/noticia.jsf?id=54097" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 col-xl-4">
+                    <div class="blog-box">
+                        <div class="blog-content">
+                            <div class="title-blog">
+                                <h3>Notícias vindo da reitoria</h3>
+                                <p>UFMA/Notícias: UFMA sediará em 2020 o XIX Encontro Nordestino dos Grupos do Programa de Educação Tutorial.</p>
+                            </div>
+                            <ul class="option-blog">
+                                <li><a href="https://portais.ufma.br/PortalUfma/paginas/noticias/noticia.jsf?id=54097" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
                 <div class="col-md-6 col-lg-4 col-xl-4">
                     <div class="blog-box">
                         <div class="blog-content">
