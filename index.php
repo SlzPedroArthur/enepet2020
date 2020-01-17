3d8887d705fe2bd22ceb446ffa552e0b379639ac
SlzPedroArthur <56669563+SlzPedroArthur@users.noreply.github.com>
2020-01-17 10:17:40 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
-                            <h1 class="m-b-20"><strong>Edital de <br>Inscriçoes!</strong></h1>
-                            <p class="m-b-40"> Clique aqui para <br> acessar o edital!</p>
+                            <h1 class="m-b-20"><strong>Edital de <br>Inscriçoes</strong></h1>
+                            <p class="m-b-40"> As inscrições para o<br>primeiro lote começarão no dia 28/01!</p>
                             <p><a class="btn hvr-hover" href="https://drive.google.com/open?id=1lB3zqhiEQHwAvxUJWPaFMYlddrbmGjZD">Edital</a></p>
                         </div>
                     </div>
@@ -32,7 +32,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
-                            <h1 class="m-b-20"><strong>Edital de Submissão <br> de Trabalhos.</strong></h1>
+                            <h1 class="m-b-20"><strong>Edital de Submissão <br> de Trabalhos</strong></h1>
                             <p class="m-b-40">Clique no link<br>abaixo para ter acesso.</p>
                             <p><a class="btn hvr-hover" href="https://even3.blob.core.windows.net/geral/EditaldeSubmissodeResumosExpandidos-XIXENEPET.bd73bb80dd104635ad42.pdf">Edital</a></p>
                         </div>
@@ -63,11 +63,50 @@
                     <div class="blog-box">
                         <div class="blog-content">
                             <div class="title-blog">
-                                <h3>Inscrições abertas!</h3>
+                                <h3> Edital de Inscrição disponível! </h3>
+                                <p>Você já pode acessar o EDITAL para ter todas as informações relacionadas ao evento.</p>
+                            </div>
+                            <ul class="option-blog">
+                                <li><a href="https://drive.google.com/file/d/1lB3zqhiEQHwAvxUJWPaFMYlddrbmGjZD/view" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 col-xl-4">
+                    <div class="blog-box">
+                        <div class="blog-content">
+                            <div class="title-blog">
+                                <h3>Edital de Vulnerabilidade Socioeconômica</h3>
+                                <p>Saiba mais como obter descontos nas inscrições! Siga todas instruções disponíveis .</p>
+                            </div>
+                            <ul class="option-blog">
+                                <li><a href="https://drive.google.com/file/d/1xF4IXhnN0AjLI1KgSxw6azCn9ZiQTPsV/view" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 col-xl-4">
+                    <div class="blog-box">
+                        <div class="blog-content">
+                            <div class="title-blog">
+                                <h3>Edital de Submissão de Trabalhos!</h3>
+                                <p></p>
+                            </div>
+                            <ul class="option-blog">
+                                <li><a href="https://even3.blob.core.windows.net/geral/EditaldeSubmissodeResumosExpandidos-XIXENEPET.bd73bb80dd104635ad42.pdf" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 col-xl-4">
+                    <div class="blog-box">
+                        <div class="blog-content">
+                            <div class="title-blog">
+                                <h3>Reunião com o Reitor da UFMA</h3>
                                 <p>UFMA/Notícias: UFMA sediará em 2020 o XIX Encontro Nordestino dos Grupos do Programa de Educação Tutorial.</p>
                             </div>
                             <ul class="option-blog">
-                                <li><a href="https://portais.ufma.br/PortalUfma/paginas/noticias/noticia.jsf?id=54097" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
+                                <li><a href="https://portais.ufma.br/PortalUfma/paginas/noticias/noticia.jsf?id=55562" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
                             </ul>
                         </div>
                     </div>
@@ -77,49 +116,23 @@
                         <div class="blog-content">
                             <div class="title-blog">
                                 <h3>Notícias vindo da reitoria</h3>
-                                <p>UFMA/Notícias: UFMA sediará em 2020 o XIX Encontro Nordestino dos Grupos do Programa de Educação Tutorial.</p>
-                            </div>
-                            <ul class="option-blog">
-                                <li><a href="https://portais.ufma.br/PortalUfma/paginas/noticias/noticia.jsf?id=54097" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 col-xl-4">
-                    <div class="blog-box">
-                        <div class="blog-content">
-                            <div class="title-blog">
-                                <h3>Notícias vindo da reitoria</h3>
-                                <p>UFMA/Notícias: UFMA sediará em 2020 o XIX Encontro Nordestino dos Grupos do Programa de Educação Tutorial.</p>
-                            </div>
-                            <ul class="option-blog">
-                                <li><a href="https://portais.ufma.br/PortalUfma/paginas/noticias/noticia.jsf?id=54097" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 col-xl-4">
-                    <div class="blog-box">
-                        <div class="blog-content">
-                            <div class="title-blog">
-                                <h3>Notícias vindo da reitoria</h3>
-                                <p>UFMA/Notícias: UFMA sediará em 2020 o XIX Encontro Nordestino dos Grupos do Programa de Educação Tutorial.</p>
-                            </div>
-                            <ul class="option-blog">
-                                <li><a href="https://portais.ufma.br/PortalUfma/paginas/noticias/noticia.jsf?id=54097" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 col-xl-4">
-                    <div class="blog-box">
-                        <div class="blog-content">
-                            <div class="title-blog">
-                                <h3>Programação saindo do forno!</h3>
                                 <p>UFMA/Notícias: Reitora recebe comissão do XIX ENEPET para planejar a realização do evento em 2020 na UFMA.</p>
                             </div>
                             <ul class="option-blog">
                                 <li><a href="https://portais.ufma.br/PortalUfma/paginas/noticias/noticia.jsf?id=54759" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 col-xl-4">
+                    <div class="blog-box">
+                        <div class="blog-content">
+                            <div class="title-blog">
+                                <h3>UFMA concorre a candidatura do XIX ENEPET</h3>
+                                <p>Então pessoal, se você ainda não sabe como entrar em contato com a gente, se liga e chama no direct do IG. Recado tá dado. </p>
+                            </div>
+                            <ul class="option-blog">
+                            <li><a href="https://portais.ufma.br/PortalUfma/paginas/noticias/noticia.jsf?id=54097" data-toggle="tooltip" data-placement="right" title="Veja mais"><i class="fas fa-eye"></i></a></li>
                             </ul>
                         </div>
                     </div>
